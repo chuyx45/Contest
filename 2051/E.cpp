e434fb990cd41d589a3aa4919f0ab933038c0eb8
chuyx45 <60621832+chuyx45@users.noreply.github.com>
2025-05-16 11:07:37 -0600

    Create E.cpp
    Best Price

---
+++ 2051/E.cpp
@@ -0,0 +1,279 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <sstream>
+#include <optional>
+#include <stdexcept>
+#include <stack>
+
+// cd /mnt/c/Users/jose/Documents/documentos_dell/Contest/
+
+using namespace __gnu_pbds;
+using namespace std;
+
+// ordered multiset
+template<typename T>
+using ordered_multiset = tree<
+	T,
+	null_type,
+	less_equal<T>,
+	rb_tree_tag,
+	tree_order_statistics_node_update>;
+
+#define rep(i, a, b) \
+	for(int i=int(a); i<int(b); ++i)
+
+#define trv(c, it) \
+	for(auto it=(c).begin(); it!=(c).end(); ++it)
+
+#define repeat(b) \
+	for(int _i=0; _i<int(b); ++_i)
+
+#define repeati(i, b) \
+	for(int i=0; i<int(b); ++i)
+
+#define LMAX LONGMAX
+#define LONG_MA 1000000000 // 10^9
+
+#define ca const auto
+#define str string
+#define vi vector<int>
+#define vl vector<long>
+#define vs vector<str>
+
+
+template <typename T>
+T read() {
+	T t;
+	cin >> t;
+	return t;
+}
+#define readi read<int>
+#define reads read<str>
+#define readl read<long>
+template <typename T>
+void print(const T&t) {
+	cout << t << '\n';
+}
+
+vi readvi(int n) {
+	vi arr(n);
+	repeati(i, n) {
+		arr[i] = readi();
+	}
+	return arr;
+}
+
+inline void no()  { cout << "NO\n";  }
+inline void yes() { cout << "YES\n"; }
+inline void check(const bool b) {
+	if (b) {
+		yes();
+	}
+	else {
+		no();
+	}
+}
+
+
+// binay_search;
+//bool binay_search(vector<int>& arr, const int l, const int r, const int x);
+//int binay_searchs(std::string& arr, const int l, const int r, const char x);
+
+constexpr int toInt(const char x) noexcept {
+	return (x + 1) - 'a';
+}
+
+constexpr bool sPair(const pair<char, int> a, const pair<char, int> b) { return a.first < b.first; }
+constexpr bool bPair(const pair<char, int> a, const pair<char, int> b) { return a.first > b.first; }
+
+struct mpair {
+	int a;
+	int b;
+};
+
+class ordered_list {
+	ordered_multiset<int> ml;
+public:
+	void insert(int x) {
+		ml.insert(x);
+	}
+	long long count_bigger_than(const int z) {
+		return ml.size() - ml.order_of_key(z);
+		auto f = ml.lower_bound(z);
+		if (f != ml.end()) {
+			ca ret = std::distance(f, ml.end());
+			return ret;
+		}
+		return 0ll;
+	}
+	vi get_b(int n) {
+		return vi(ml.begin(), ml.end());
+	}
+};
+
+bool sort_by_b2(mpair x, mpair y) {
+	if (x.b == y.b) {
+		return x.a < y.a;
+	}
+	return x.b < y.b;
+}
+bool sort_by_a2(mpair x, mpair y) {
+	if (x.a == y.a) {
+		return x.b < y.b;
+	}
+	return x.a < y.a;
+}
+long long count_bigger_than(const std::vector<mpair>& arr, const int z) {
+	mpair z1 = { z, z };
+	auto f = std::lower_bound(arr.begin(), arr.end(), z1, sort_by_a2);
+	if (f != arr.end()) {
+		ca ret = std::distance(f, arr.end());
+		return ret;
+	}
+	return 0ll;
+}
+
+int main() {
+	ca r = readi();
+	int azsx = 0;
+	repeat(r) {
+		++azsx;
+		int n = readi();
+		ca k = read<long long>();
+		vector<mpair> arr(n);
+		repeati(i, n) {
+			arr[i].a = readi();
+		}
+		repeati(i, n) {
+			arr[i].b = readi();
+		}
+
+		sort(arr.begin(), arr.end(), sort_by_a2);
+		long long ax = 0l;
+		ordered_list l;
+		repeati(i, n) {
+			ca count = l.count_bigger_than(arr[i].a);
+			//std::cout << count << " c:n-i " << (n - i) << '\n';
+			if (count <= k) {
+				const long long asdx = (count + static_cast<long long>(n - i)) * static_cast<long long>(arr[i].a);
+				ax = max(ax, asdx);
+			}
+			l.insert(arr[i].b);
+			//std::cout << ax << " max1\n";
+		}
+
+		auto barr = l.get_b(n);
+		int last_ = -1;
+		repeati(i, n) {
+			auto count = count_bigger_than(arr, barr[i]);
+			long long y = (n - i) - count;
+			if (y <= k and last_ != barr[i]) {
+				count += y;
+				ax = max(ax, static_cast<long long>(barr[i]) * count);
+			}
+			last_ = barr[i];
+		}
+		std::cout << ax << '\n';
+	}
+	return 0;
+}
+
+
+bool binay_search(vector<int>& arr, const int l, const int r, const int x) {
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
+		if (arr[mid] == x) {
+			return true;
+		}
+		if (arr[mid] > x) {
+			return binay_search(arr, l, mid - 1, x);
+		}
+		return binay_search(arr, mid + 1, r, x);
+	}
+	return false;
+}
+
+int binay_searchs(std::string& arr, const int l, const int r, const char x) {
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
+		if (arr[mid] == x) {
+			return mid;
+		}
+		if (arr[mid] > x) {
+			return binay_searchs(arr, l, mid - 1, x);
+		}
+		return binay_searchs(arr, mid + 1, r, x);
+	}
+	return -1;
+}
+
+str reverse(const str& a) {
+	str b = a;
+	for (int i = 0; i < a.size(); ++i) {
+		b[b.size() - (i + 1)] = a[i];
+	}
+	return b;
+}
+int m_lex_compare(const str& a) {
+	ca a_rev = reverse(a);
+	for (int i = 0; i < a.size(); ++i) {
+		if (a[i] > a_rev[i]) {
+			return -1;
+		}
+		else if (a[i] < a_rev[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+class pair_permut {
+	int index_a = 0;
+	int index_b = 1;
+public:
+	optional<pair<int, int>> get_next(const vi& a) {
+		if (index_b >= a.size()) {
+			++index_a;
+			index_b = index_a + 1;
+		}
+		if (index_b >= a.size()) {
+			return {};
+		}
+		pair<int, int> r(a[index_a], a[index_b]);
+		++index_b;
+		return r;
+	}
+};
+
+int gcd(const int a, const int b) {
+	int x = min(a, b);
+	while (x >= 2) {
+		if ((a % x) == 0 and (b % x) == 0) {
+			return x;
+		}
+		--x;
+	}
+	return 1;
+}
+
+template <class T>
+bool all_eq_t(/*iterator<T> it, const T val*/) {
+	return true;
+}
+
+bool all_eq(const std::string& a) {
+	const char c = a[0];
+	for (ca x : a) {
+		if (x != c) {
+			return false;
+		}
+	}
+	return true;
+}
